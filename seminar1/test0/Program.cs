1291388e09759fe4431552912c6c1ba9acdbd00c
Elena Lukina <e05061981@rambler.ru>
2023-06-11 00:32:30 +0300

    завершили работу над первым семинаром

---
+++ seminar1/test0/Program.cs
@@ -0,0 +1,35 @@
+﻿// //  Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3, b = 9 -> нет
+// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 	3 -> Среда 
+// 5 -> Пятница
+
+using System;
+
+Console.WriteLine("введите первое число:");
+string? numberStringOne = Console.ReadLine();
+
+Console.WriteLine("введите второе число:");
+string? numberStringTwo = Console.ReadLine();
+
+int numberIntOne = Convert.ToInt32(numberStringOne);
+int numberIntTwo = Convert.ToInt32(numberStringTwo);
+if (numberIntTwo * numberIntTwo == numberIntOne)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
+
+
+
+
+
+    
---
+++ seminar1/test1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 	3 -> Среда 
+// 5 -> Пятница
+using System;
+
+Console.WriteLine("Введите день недели от 1 до 7:");
+string? numberString = Console.ReadLine();
+int numberInt = Convert.ToInt32(numberString);
+
+if (numberInt ==1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (numberInt ==2)
+{
+    Console.WriteLine("Вторник");
+}
+     if (numberInt ==3)
+{
+    Console.WriteLine("среда");
+}
+if (numberInt ==4)
+{
+    Console.WriteLine("Четверг");
+}
+if (numberInt ==5)
+{
+    Console.WriteLine("Пятница");
+}
+if (numberInt ==6)
+{
+    Console.WriteLine("Суббота");
+}
+if (numberInt ==7)
+{
+        Console.WriteLine("Воскресенье");
+}
+if (numberInt > 8)
+{
+Console.WriteLine("Такого ещё не придумали"); 
+}   
+
+   
