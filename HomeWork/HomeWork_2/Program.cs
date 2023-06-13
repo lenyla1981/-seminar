345ed58f079459c54f2d6fef2bad522bf00233bd
Elena Lukina <e05061981@rambler.ru>
2023-06-13 19:57:24 +0300

    завершили работу над первым домашним заданием

---
+++ HomeWork/HomeWork_2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+
+
+ Console.WriteLine("Введите три числа:");
+ 
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+
+if (a > b) 
+ {
+    max = a;
+ }
+else
+ {
+      max = b;
+ }
+if (b > c)
+ {
+     max = b;
+ }
+else
+ {
+     max = c;
+ }
+if (c > a)
+ {
+     max = c;
+ }
+else
+ {
+     max = a;
+ }
+Console.Write("max=" +max);
