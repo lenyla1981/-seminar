90d4e8802fca1ea2ed0ba2a6e162bfb78bbedc40
Elena Lukina <e05061981@rambler.ru>
2023-06-17 23:48:18 +0300

    завершили работу над третьим домашним заданием

---
+++ HomeWork_2/work_1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+
+    int Prompt(string msg)
+
+    {
+    System.Console.WriteLine($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());   
+    }
+    int number = Prompt("Введите трёхзначное число");
+    if (number >=100 && number <1000)
+
+    {
+    int mid = (number % 100 - number % 10)/10;
+    System.Console.WriteLine($"Вторая цифра числа{number} является {mid}");
+    }
+    else
+    System.Console.WriteLine("Это число не трёхзначное");
+    
+    
+    
+ 
+
+
---
+++ HomeWork_2/work_333/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да 
+// 7 -> да 
+// 1 -> нет
+
+using System;
+
+Console.WriteLine("Введите день недели от 1 до 7:");
+string? numberString = Console.ReadLine();
+int numberInt = Convert.ToInt32(numberString);
+
+if (numberInt ==1)
+{
+    Console.WriteLine("нет");
+}
+if (numberInt ==2)
+{
+    Console.WriteLine("нет");
+}
+     if (numberInt ==3)
+{
+    Console.WriteLine("нет");
+}
+if (numberInt ==4)
+{
+    Console.WriteLine("нет");
+}
+if (numberInt ==5)
+{
+    Console.WriteLine("нет");
+}
+if (numberInt ==6)
+{
+    Console.WriteLine("Да");
+}
+if (numberInt ==7)
+{
+        Console.WriteLine("Да");
+}
+if (numberInt > 8)
+{
+Console.WriteLine("Такого ещё не придумали"); 
+}   
