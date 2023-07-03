0364b421b80dfcbe9c9275901bc62a9ff07ab4e3
Elena Lukina <e05061981@rambler.ru>
2023-07-03 19:58:52 +0300

    завершили работу над седьмым HomeWork

---
+++ HomeWork_7/task 47/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+ const int ROWS = 3;
+const int COLUMNS = 4;
+
+int[,] GetRandomMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j< matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = matrix[i, j] = Random.Shared.Next(-100, 100) / 10;;
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+ for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j< matrix.GetLength(1); j++)
+       {
+        Console.Write($"{matrix[i, j]} ");
+       }
+       Console.WriteLine();
+    }
+}
+
+int[,] mymatrix = GetRandomMatrix(ROWS, COLUMNS);
+PrintMatrix(mymatrix);
+
+
+
+
+// Console.WriteLine("Введите количество строк  ");
+// int linesVol = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int columnsVol = Convert.ToInt32(Console.ReadLine());
+// double[,] numbers = new double[linesVol, columnsVol];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+// void FillArrayRandomNumbers(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)     
+//         {
+//             array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+//         }
+//     }
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine("");
+//     }
+// }
---
+++ HomeWork_7/task 52/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,7; 5,7; 3,7; 3.
+
+
+  Console.Write("Введите размерность m массива: ");
+  int m = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите размерность n массива: ");
+  int n = Convert.ToInt32(Console.ReadLine());
+  int[,] randomArray = new int[m,n];
+
+  void mas(int m, int n)  
+  {
+  int i,j;
+  Random matrix = new Random();
+  for (i = 0; i < m; i++)
+  {
+  Console.WriteLine();
+  for (j = 0; j < n; j++)
+  {
+  randomArray[i,j] = matrix.Next(1,9);
+  Console.Write($"{randomArray[i,j]} ");
+  }
+  Console.WriteLine();
+  }
+  }
+
+  void arif(int m, int n)
+  { 
+     Console.Write("Среднее арифметическое каждого столбца: ");
+  int i,j;
+  Random matrix = new Random();
+  for (j = 0; j < n; j++)
+  {
+  double sum = 0;
+  for (i = 0; i < m; i++)
+  {
+  sum = sum + randomArray[i,j];
+  }
+  Console.Write($"{sum/(i):F1}; ");
+  }
+  }
+  mas(m,n);
+  arif(m,n);
+
