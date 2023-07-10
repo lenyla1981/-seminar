15c90f7ebe3a35cf557aa33fd0bbc34d1869bdfd
Elena Lukina <e05061981@rambler.ru>
2023-07-10 22:08:31 +0300

    завершили работу над девятым HomeWork

---
+++ HomeWorks/HomeWork_9/task 64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+int number = 6;
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0} ", number);
+    NumberCounter (number - 1);
+}
+NumberCounter(number);
---
+++ HomeWorks/HomeWork_9/task 66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в 
+//промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int numberM = 1;
+int numberN = 15;
+void Perechislenie(int LastNumber, int FirstNumber, int sum)
+{
+    if (LastNumber > FirstNumber ) 
+    {
+    Console.WriteLine(sum); 
+    return;
+    }
+    sum = sum + (numberM++);
+   Perechislenie(numberM, numberN, sum);
+}
+
+Perechislenie(numberM, numberN, 0); 
+    
+
